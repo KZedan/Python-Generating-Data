a06dc7a20fb31d391e25da311cff8a684dd1b714
Kareem <kareem335@yahoo.co.uk>
2019-09-16 21:59:33 +0100

    setting x values and y values for a scatter plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.scatter(2, 4, s=200)
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
